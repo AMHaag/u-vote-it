1584da7d2515145d01570ac1f82acb8167d19f70
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-06-20 21:54:21 -0400

    new delete and get routes

--- server.js
+++ server.js
@@ -45,6 +45,58 @@ const db = mysql.createConnection(
 //   console.log(result);
 // });
 
+//Get all candidates
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+app.get("/api/candidates/:id", (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id=?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+app.delete("/api/candidates/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id=?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Candidate not found",
+      });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
