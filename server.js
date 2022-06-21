dd1926736f5d00a8a92dcaf1fade2b2be42be522
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-06-21 00:47:44 -0400

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const inputCheck = require("./utils/inputCheck");
 const PORT = process.env.PORT || 7121;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -97,6 +98,32 @@ app.delete("/api/candidates/:id", (req, res) => {
   });
 });
 
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name,industry_connected)
+               VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
